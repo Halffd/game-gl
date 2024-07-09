3e4d5e4664e7b8e0508dccdd519bb697374bd822
Halffd <guhalfeld@hotmail.com>
2024-07-08 22:01:39 -0300

    vertical lines

--- shaders/shader.fs
+++ shaders/shader.fs
@@ -8,10 +8,10 @@ uniform sampler2D texture2;
 uniform vec4 mixColor;
 
 
-vec4 checkerboard(vec2 uv, float size) {
+vec4 verticalLines(vec2 uv, float size, vec4 color1, vec4 color2) {
     vec2 grid = floor(uv * size);
-    float checker = grid.x /+ grid.y; //mod(grid.x + grid.y, 2.0);
-    return vec4(checker, checker, checker, 1.0);
+    float checker = mod(grid.x, 2.0);
+    return mix(color1, color2, checker);
 }
 
 void main()
@@ -19,5 +19,5 @@ void main()
     vec4 tex1 = texture(texture1, TexCoord);
     vec4 tex2 = texture(texture2, TexCoord);
     vec4 res = mix(tex1, tex2, 0.2);
-    FragColor = checkerboard(TexCoord, 10.0);
+    FragColor = verticalLines(TexCoord, 10.0, vec4(1.0), vec4(0.0));
 }
