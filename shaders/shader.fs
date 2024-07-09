a3aba4782d402472db8a79896905ce177b2bac9b
Halffd <guhalfeld@hotmail.com>
2024-07-08 21:37:59 -0300

    gradient

--- shaders/shader.fs
+++ shaders/shader.fs
@@ -7,19 +7,15 @@ uniform sampler2D texture1;
 uniform sampler2D texture2;
 uniform vec4 mixColor;
 
+vec4 gradient(vec2 uv, vec4 color1, vec4 color2) {
+    return mix(color1, color2, uv.x);
+}
+
 void main()
 {
-    vec4 color1 = texture(texture1, TexCoord);
-    vec4 color2 = texture(texture2, TexCoord);
-    
-    // Calculate the average of the RGB channels for each color
-    float average1 = (color1.r + color1.g + color1.b) / 3.0;
-    float average2 = (color2.r + color2.g + color2.b) / 3.0;
-    
-    // Mix the two grayscale values based on the mix factor
-    float mixFactor = 0.2; // Adjust the mix factor as desired
-    float mixedAverage = mix(average1, average2, mixFactor);
-    
-    // Set the final color to grayscale
-    FragColor = vec4(mixedAverage, mixedAverage, mixedAverage, 1.0);
+    vec4 tex1 = texture(texture1, TexCoord);
+    vec4 tex2 = texture(texture2, TexCoord);
+    vec4 res = mix(tex1,tex2,0.2);
+    FragColor = gradient(TexCoord, res, vec4(0.0,0.0,1.0,0.0));
+
 }
