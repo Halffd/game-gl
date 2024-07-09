910b54d75b3454bcc98d920fbeb303447cc2b5c1
Halffd <guhalfeld@hotmail.com>
2024-07-08 21:40:47 -0300

    circular gradient

--- shaders/shader.fs
+++ shaders/shader.fs
@@ -7,15 +7,15 @@ uniform sampler2D texture1;
 uniform sampler2D texture2;
 uniform vec4 mixColor;
 
-vec4 gradient(vec2 uv, vec4 color1, vec4 color2) {
-    return mix(color1, color2, uv.x);
+vec4 circularGradient(vec2 uv, vec4 color1, vec4 color2) {
+    float dist = length(uv - vec2(0.5, 0.5)); // Calculate the distance from the center (0.5, 0.5)
+    return mix(color1, color2, dist);
 }
 
 void main()
 {
     vec4 tex1 = texture(texture1, TexCoord);
     vec4 tex2 = texture(texture2, TexCoord);
-    vec4 res = mix(tex1,tex2,0.2);
-    FragColor = gradient(TexCoord, res, vec4(0.0,0.0,1.0,0.0));
-
+    vec4 res = mix(tex1, tex2, 0.2);
+    FragColor = circularGradient(TexCoord, res, vec4(0.0, 0.0, 0.0, 1.0));
 }
