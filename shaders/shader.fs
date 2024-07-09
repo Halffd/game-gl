9df3bf19f3020469689fe256afa95169d1e86052
Halffd <guhalfeld@hotmail.com>
2024-07-08 21:46:56 -0300

    checkerboard grid

--- shaders/shader.fs
+++ shaders/shader.fs
@@ -7,9 +7,11 @@ uniform sampler2D texture1;
 uniform sampler2D texture2;
 uniform vec4 mixColor;
 
-vec4 circularGradient(vec2 uv, vec4 color1, vec4 color2) {
-    float dist = length(uv - vec2(0.5, 0.5)); // Calculate the distance from the center (0.5, 0.5)
-    return mix(color1, color2, dist);
+
+vec4 checkerboard(vec2 uv, float size) {
+    vec2 grid = floor(uv * size);
+    float checker = mod(grid.x + grid.y, 2.0);
+    return vec4(checker, checker, checker, 1.0);
 }
 
 void main()
@@ -17,5 +19,5 @@ void main()
     vec4 tex1 = texture(texture1, TexCoord);
     vec4 tex2 = texture(texture2, TexCoord);
     vec4 res = mix(tex1, tex2, 0.2);
-    FragColor = circularGradient(TexCoord, res, vec4(0.0, 0.0, 0.0, 1.0));
+    FragColor = checkerboard(TexCoord, 10.0);
 }
