51ee342b19a1e9f3c35bcd9282babe63a514f8c2
Halffd <guhalfeld@hotmail.com>
2024-09-12 11:44:58 -0300

    directional light

--- shaders/fragment.glsl
+++ shaders/fragment.glsl
@@ -15,7 +15,7 @@ struct Material {
     float     shininess;
 };
 struct Light {
-    vec3 position;
+    vec3 direction;
 
     vec3 ambient;
     vec3 diffuse;
@@ -31,14 +31,12 @@ uniform float darkness;
 uniform float steps;
 
 uniform vec3 objectColor;
-uniform vec3 lightColor;
-uniform vec3 lightPos;
 uniform vec3 viewPos;
 
 void main()
 {
     vec3 norm = normalize(Normal);
-    vec3 lightDir = normalize(lightPos - FragPos);
+    vec3 lightDir = normalize(-light.direction);
     float diff = max(dot(norm, lightDir), 0.0);
     //    float steps = 7.2; // Adjust this value to control the number of shading levels
     vec3 diffuse = light.diffuse * diff * vec3(texture(material.diffuse, TexCoord));
--- src/main.cpp
+++ src/main.cpp
@@ -152,99 +152,6 @@ glm::vec3 lightPos(1.2f, 1.0f, 2.0f);
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
 
-// The Width of the screen
-const unsigned int SCREEN_WIDTH = 800;
-// The height of the screen
-const unsigned int SCREEN_HEIGHT = 600;
-
-Game Breakout(SCREEN_WIDTH, SCREEN_HEIGHT);
-
-int game(int argc, char *argv[])
-{
-    glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-#ifdef __APPLE__
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-#endif
-    glfwWindowHint(GLFW_RESIZABLE, false);
-
-    GLFWwindow* window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Breakout", nullptr, nullptr);
-    glfwMakeContextCurrent(window);
-
-    // glad: load all OpenGL function pointers
-    // ---------------------------------------
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-    {
-        std::cout << "Failed to initialize GLAD" << std::endl;
-        return -1;
-    }
-
-    glfwSetKeyCallback(window, key_callback);
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-
-    // OpenGL configuration
-    // --------------------
-    glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
-    glEnable(GL_BLEND);
-    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-
-    // initialize game
-    // ---------------
-    Breakout.Init();
-
-    // deltaTime variables
-    // -------------------
-    float deltaTime = 0.0f;
-    float lastFrame = 0.0f;
-
-    while (!glfwWindowShouldClose(window))
-    {
-        // calculate delta time
-        // --------------------
-        float currentFrame = glfwGetTime();
-        deltaTime = currentFrame - lastFrame;
-        lastFrame = currentFrame;
-        glfwPollEvents();
-
-        // manage user input
-        // -----------------
-        Breakout.ProcessInput(deltaTime);
-
-        // update game state
-        // -----------------
-        Breakout.Update(deltaTime);
-
-        // render
-        // ------
-        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
-        Breakout.Render();
-
-        glfwSwapBuffers(window);
-    }
-
-    // delete all resources as loaded using the resource manager
-    // ---------------------------------------------------------
-    ResourceManager::Clear();
-
-    glfwTerminate();
-    return 0;
-}
-void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
-{
-    // when a user presses the escape key, we set the WindowShouldClose property to true, closing the application
-    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, true);
-    if (key >= 0 && key < 1024)
-    {
-        if (action == GLFW_PRESS)
-            Breakout.Keys[key] = true;
-        else if (action == GLFW_RELEASE)
-            Breakout.Keys[key] = false;
-    }
-}
 // glfw: whenever the mouse scroll wheel scrolls, this callback is called
 // ----------------------------------------------------------------------
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
@@ -388,16 +295,17 @@ void renderScene(GLFWwindow *window, std::vector<VAO *> &meshes)
     float t = glfwGetTime();
     float dark = clamp(t, cos, 0.5f, 1.0f);
     shader.SetFloat("darkness",  dark);
-    glm::vec3 lightColor = glm::vec3(dark, dark, dark);
-    shader.SetVector3f("lightColor",  lightColor);
     float radius = 3.0f;
     float angularSpeed = 0.5f;
 
-    lightPos.x = radius * cos(angularSpeed * t);
-    lightPos.y = radius * sin(angularSpeed * t);
-    lightPos.z = radius * sin(angularSpeed * t * 0.5f);
-    shader.SetVector3f("lightPos", lightPos);
+    //lightPos.x = radius * cos(angularSpeed * t);
+    //lightPos.y = radius * sin(angularSpeed * t);
+    //lightPos.z = radius * sin(angularSpeed * t * 0.5f);
+    lightPos = glm::vec3(-0.2f, -1.0f, -0.3f);
+    shader.SetVector3f("light.direction", lightPos);
     shader.SetVector3f("viewPos", camera.Position);
+
+    glm::vec3 lightColor = glm::vec3(dark, dark, dark);
 
     Shader light = ResourceManager::GetShader("light");
     light.Use();
